0b1e6935b7e1ada2b71bf4f73ab37830cea442b3
Wynand Winterbach <wynand@translate.org.za>
2008-04-11 14:53:03 +0000

    Added Dwayne's changes.
    svn path=/src/trunk/virtaal/; revision=7161

--- xgettext.sh
+++ xgettext.sh
@@ -3,4 +3,4 @@
 xgettext --msgid-bugs-address=translate-devel@lists.sourceforge.net \
          --copyright-holder="Zuza Software Foundation (Translate.org.za)" \
          --output=po/virtaal.pot \
-         $(find virtaal/ -name "*.py" -o -name "*.glade")
+         $(find virtaal/ -name "*.py" -o -name "*.glade" | sort)

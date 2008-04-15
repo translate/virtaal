ba22aa7014ec16b074fcea43bfb3ff45151fdcf3
Friedel Wolff <friedel@translate.org.za>
2008-04-15 10:04:18 +0000

    Add and update some copyright headers
    svn path=/src/trunk/virtaal/; revision=7200

--- virtaal/undo_buffer.py
+++ virtaal/undo_buffer.py
@@ -3,7 +3,7 @@
 # 
 # Copyright 2008 Zuza Software Foundation
 # 
-# This file is part of translate.
+# This file is part of virtaal.
 #
 # translate is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

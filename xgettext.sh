f95ea16359f7f6cf46a6d79afa5cfe87b8eaffbe
Dwayne Bailey <dwayne@translate.org.za>
2008-04-16 15:10:48 +0000

    Remove old translation extraction script
    svn path=/src/trunk/virtaal/; revision=7207

--- xgettext.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xgettext --msgid-bugs-address=translate-devel@lists.sourceforge.net \
-         --copyright-holder="Zuza Software Foundation (Translate.org.za)" \
-         --output=po/virtaal.pot \
-         $(find virtaal/ -name "*.py" -o -name "*.glade" | sort)

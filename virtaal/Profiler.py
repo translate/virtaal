542ed141a9078cd0078bb7a1da6b05f793509bd2
Friedel Wolff <friedel@translate.org.za>
2008-04-29 07:52:51 +0000

    Update license block
    svn path=/src/trunk/virtaal/; revision=7264

--- virtaal/Profiler.py
+++ virtaal/Profiler.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2007 The authors of Jokosher
+# Copyright 2008 Zuza Software Foundation
+# 
+# This file was part of Jokosher.
+# This file is part of virtaal.
+#
+# virtaal is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#
-#    THIS FILE IS PART OF THE JOKOSHER PROJECT AND LICENSED UNDER THE GPL. SEE
-#    THE 'COPYING' FILE FOR DETAILS
-#
-#    This module is meant for testing and profiling the code only.
-#    This file should not be included in any release.
-#
-#-------------------------------------------------------------------------------
+"""This module is meant for testing and profiling the code only. It should not 
+be included in any release."""
 
 import hotshot
 from hotshot import stats

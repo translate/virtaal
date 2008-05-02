c9a0e6ae99a8be55d96dc8c7f2667f93427fd24c
Friedel Wolff <friedel@translate.org.za>
2008-05-02 07:11:47 +0000

    Correct copyright header
    svn path=/src/trunk/virtaal/; revision=7310

--- virtaal/support/bijection.py
+++ virtaal/support/bijection.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 Zuza Software Foundation
+# Copyright 2008 Zuza Software Foundation
 #
 # This file is part of virtaal.
 #
-# translate is free software; you can redistribute it and/or modify
+# This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# translate is distributed in the hope that it will be useful,
+# This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.

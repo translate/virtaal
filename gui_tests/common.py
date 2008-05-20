0745f259d010318566840a116beaef48e256faf3
Wynand Winterbach <wynand@translate.org.za>
2008-05-20 15:16:21 +0000

    Some further big refactorings of the test code. There's nothing really new here, other than that the tests are now encapsulated in classes.
    svn path=/src/trunk/virtaal/; revision=7452

--- gui_tests/common.py
+++ gui_tests/common.py
@@ -1,15 +1,83 @@
+from translate.convert import pot2po
 import os
 from os import path, environ
 
 from dogtail import tree, config
 from dogtail.utils import run
 
+import po2pot
+
 environ['LANGUAGE']='en_US.UTF-8'
 config.config.defaults['absoluteNodePaths'] = True
 
-def run_virtaal(config_file):
-    run("../run_virtaal.py --config=%s" % path.abspath(config_file), timeout=0)
-    return tree.root.application("run_virtaal.py")
+def find_app(exe_name):
+    full_exe_name = path.abspath(exe_name)
+    full_exe_dir_name = path.dirname(full_exe_name)
+    
+    # If the parent of the current pathname is the same as the current pathname,
+    # we've hit the topmost directory and we give up, returning None
+    if full_exe_dir_name == path.abspath(path.join(full_exe_dir_name, "..")):
+        return None
+    
+    if path.exists(full_exe_name):
+        return full_exe_name
+    else:
+        return find_app(path.join("..", exe_name))    
+
+class BaseGuiTest(object):
+    """Tests running actual commands on files"""
+    defaultoptions = {}
+    virtaal_cmd = "run_virtaal.py"
+    
+    def __init__(self):
+        self.config_file = None
+        self.testdir = None
+        self.rundir = None
+    
+    def abspath(self, partial_fname):
+        return path.abspath(path.join(self.testdir, partial_fname))
+    
+    def setup_method(self, method):
+        """creates a clean test directory for the given method"""
+        self.testdir = "%s_%s" % (self.__class__.__name__, method.__name__)
+        self.cleardir()
+        os.mkdir(self.testdir)
+        self.rundir = os.path.abspath(os.getcwd())
+
+    def teardown_method(self, _method):
+        """removes the test directory for the given method"""
+        os.chdir(self.rundir)
+        self.cleardir()
+
+    def cleardir(self):
+        """removes the test directory"""
+        if os.path.exists(self.testdir):
+            for dirpath, subdirs, filenames in os.walk(self.testdir, topdown=False):
+                for name in filenames:
+                    os.remove(os.path.join(dirpath, name))
+                for name in subdirs:
+                    os.rmdir(os.path.join(dirpath, name))
+        if os.path.exists(self.testdir): os.rmdir(self.testdir)
+        assert not os.path.exists(self.testdir)
+
+    def run(self, *argv, **kwargs):
+        """runs the command via the main function, passing self.defaultoptions and keyword arguments as --long options and argv arguments straight"""
+        os.chdir(self.testdir)
+        argv = list(argv)
+        kwoptions = getattr(self, "defaultoptions", {}).copy()
+        kwoptions.update(kwargs)
+        for key, value in kwoptions.iteritems():
+            if value is True:
+                argv.append("--%s" % key)
+            else:
+                argv.append("--%s=%s" % (key, value))
+
+        cmd = "%s %s" % (find_app(self.virtaal_cmd), " ".join(argv))
+        run(cmd, timeout=0)
+        return self.get_app()
+
+    def get_app(self):
+        return tree.root.application(self.virtaal_cmd)
 
 def click_file_open(node):
     node.menuItem("File").menuItem("Open").click()
@@ -44,3 +112,37 @@ def gui_saveas(node, filename):
 
 def gui_quit(node):
     node.menuItem("File").menuItem("Quit").click()
+
+def test_fname(dirname, filename):
+    return path.join(dirname, filename)
+
+def ensure_dir_exists(dirname):
+    if path.exists(dirname) and not path.isdir(dirname):
+        os.unlink(dirname)
+    if not path.exists(dirname):
+        os.makedirs(dirname)
+
+def enter_test_dir(test_name):
+    ensure_dir_exists(test_name)
+    os.chdir(test_name)
+
+def write_file(filename, data):
+    f = open(filename, 'w+')
+    try:
+        f.write(data)
+        return filename
+    finally:
+        f.close()
+    return filename
+
+def read_file(filename):
+    return open(filename).read()
+
+def strip_translations(po_filename):
+    fname, ext = path.splitext(po_filename)
+    if ext != '.po':
+        raise NameError("The extension should be .pot")
+    pot_filename = fname + ".pot"
+    po2pot.main([po_filename, pot_filename])
+    return pot_filename
+    
--- gui_tests/test_open_and_close.py
+++ gui_tests/test_open_and_close.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
 from time import sleep
-import os
-from os import path
 import common
 
 def create_ini_file(filename):
-    ini_file = open(path.abspath(filename), "w+")
-    ini_file.write("""
+    return common.write_file(filename, """
 [translator]
 email = 
 name = 
@@ -23,9 +20,14 @@ sourcelang = en
 
 [general]
 windowheight = 620
-windowwidth = 400
-    """)
-    ini_file.close()
+windowwidth = 400""")
+
+def create_po_file(filename):
+    return common.write_file(filename, """
+#, fuzzy    
+msgid "Hello, world!"
+msgstr "Hello, wereld!"
+""")
 
 def handle_popup_query(node, dialog_title, input_text):
     wnd = node.child(name=dialog_title)
@@ -34,27 +36,30 @@ def handle_popup_query(node, dialog_title, input_text):
     ok_button = wnd.button(buttonName="OK")
     ok_button.click()
 
-def test_basic():
-    os.chdir(path.dirname(__file__))
-    
-    if path.exists("hello_world.po"):
-        os.unlink("hello_world.po")
+class TestOpenAndClose(common.BaseGuiTest):
+    def test_open_and_close(self):
+        CONFIG_FILE = self.abspath("config.ini")
+        PO_FILE = self.abspath("hello_world.po")
+        TEST_PO_FILE = self.abspath("test_hello_world.po")
 
-    create_ini_file("test_open_and_close.ini")
-    virtaal = common.run_virtaal("test_open_and_close.ini")
-    common.gui_openfile(virtaal, "hello_world.pot")
-    
-    edit_area = virtaal.child(roleName='table cell')
-    sleep(1)
-    edit_area.typeText("Hello, wereld!")
-    edit_area.keyCombo("<Alt>u")
+        virtaal = self.run(config=create_ini_file(CONFIG_FILE))
+                
+        po_file = create_po_file(PO_FILE)
+        common.gui_openfile(virtaal, common.strip_translations(po_file))
         
-    common.gui_saveas(virtaal, "hello_world.po")
-    handle_popup_query(virtaal, "Please enter your name", "Hello Person")
-    handle_popup_query(virtaal, "Please enter your e-mail address", "hello@world.com")
-    handle_popup_query(virtaal, "Please enter your team's information", "Hello Team")
+        edit_area = virtaal.child(roleName='table cell')
+        sleep(1)
+        edit_area.typeText("Hello, wereld!")
+        edit_area.keyCombo("<Alt>u")
+        
+        common.gui_saveas(virtaal, TEST_PO_FILE)
+        handle_popup_query(virtaal, "Please enter your name", "Hello Person")
+        handle_popup_query(virtaal, "Please enter your e-mail address", "hello@world.com")
+        handle_popup_query(virtaal, "Please enter your team's information", "Hello Team")
+        
+        common.gui_quit(virtaal)
     
-    common.gui_quit(virtaal)
+        print common.read_file(TEST_PO_FILE)
     
-    contents = open("hello_world.po").read().split("\n\n")
-    assert contents[1] == '#, fuzzy\nmsgid "Hello, world!"\nmsgstr "Hello, wereld!"\n'
+#    contents = open("hello_world.po").read().split("\n\n")
+#    assert contents[1] == '#, fuzzy\nmsgid "Hello, world!"\nmsgstr "Hello, wereld!"\n'

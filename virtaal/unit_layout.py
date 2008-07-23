ef8b1fffa2b06e64a7b8b572395d308dd114d9fa
Wynand Winterbach <wynand@translate.org.za>
2008-07-23 13:18:02 +0000

    Moved widget creation code to unit_layout.py.
    svn path=/src/trunk/virtaal/; revision=7836

--- virtaal/unit_layout.py
+++ virtaal/unit_layout.py
@@ -20,9 +20,22 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 from itertools import chain
+import logging
+import re
 
-from support.partial import partial
+import gtk
+try:
+    import gtkspell
+except ImportError, e:
+    gtkspell = None
+
+import pan_app
 from pan_app import _
+from support.partial import partial
+from support.simplegeneric import generic
+import markup
+import undo_buffer
+from widgets import label_expander 
 
 class Widget(object):
     def __init__(self, name):
@@ -82,6 +95,172 @@ class Option(Widget):
         self.label = label
         self.get_option = get_option
         self.set_option = set_option
+
+def on_key_press_event(widget, event, *_args):
+    if event.keyval == gtk.keysyms.Return or event.keyval == gtk.keysyms.KP_Enter:
+        widget.parent.emit('key-press-event', event)
+        return True
+    return False
+
+@generic
+def make_widget(layout):
+    raise NotImplementedError()
+
+def post_make_widget(widget, names, layout):
+    layout.__widget = widget
+    widget.__layout = layout
+    # Skip Enter key processing
+    widget.connect('key-press-event', on_key_press_event)
+    return widget, names
+
+def get_layout(widget):
+    return widget.__layout
+
+def get_widget(layout):
+    return layout.__widget
+
+@make_widget.when_type(Layout)
+def make_layout(layout):
+    table = gtk.Table(rows=1, columns=4, homogeneous=True)
+    names = {layout.name: table}
+    child, child_names = make_widget(layout.child)
+    table.attach(child, 1, 3, 0, 1, xoptions=gtk.FILL|gtk.EXPAND, yoptions=gtk.FILL)
+    names.update(child_names)
+
+    return post_make_widget(table, names, layout)
+
+def fill_list(lst, box):
+    names = {lst.name: box}
+    for child in lst.children:
+        child_widget, child_names = make_widget(child)
+        box.pack_start(child_widget, fill=True, expand=False)
+        names.update(child_names)
+    #box.connect('key-press-event', on_key_press_event)
+    return box, names
+
+@make_widget.when_type(VList)
+def make_vlist(layout):
+    box, names = fill_list(layout, gtk.VBox())
+    return post_make_widget(box, names, layout)
+
+@make_widget.when_type(HList)
+def make_hlist(layout):
+    box, names = fill_list(layout, gtk.HBox())
+    return post_make_widget(box, names, layout)
+
+#A regular expression to help us find a meaningful place to position the
+#cursor initially.
+first_word_re = re.compile("(?m)(?u)^(<[^>]+>|\\\\[nt]|[\W$^\n])*(\\b|\\Z)")
+
+def focus_text_view(text_view):
+    text_view.grab_focus()
+
+    buf = text_view.get_buffer()
+    text = buf.get_text(buf.get_start_iter(), buf.get_end_iter())
+
+    translation_start = first_word_re.match(text).span()[1]
+    buf.place_cursor(buf.get_iter_at_offset(translation_start))
+
+def add_spell_checking(text_view, language):
+    global gtkspell
+    if gtkspell:
+        try:
+            spell = gtkspell.Spell(text_view)
+            spell.set_language(language)
+        except:
+            logging.info(_("Could not initialize spell checking"))
+            gtkspell = None
+
+@make_widget.when_type(SourceTextBox)
+def make_source_text_box(layout):
+    text_view = gtk.TextView()
+
+    add_spell_checking(text_view, pan_app.settings.language["sourcelang"])
+
+    text_view.get_buffer().set_text(markup.escape(layout.get_text()))
+    text_view.set_editable(layout.editable)
+    text_view.set_wrap_mode(gtk.WRAP_WORD)
+    text_view.set_border_window_size(gtk.TEXT_WINDOW_TOP, 1)
+
+    scrolled_window = gtk.ScrolledWindow()
+    scrolled_window.set_policy(gtk.POLICY_NEVER, gtk.POLICY_NEVER)
+    scrolled_window.add(text_view)
+
+    return post_make_widget(scrolled_window, {layout.name: scrolled_window}, layout)
+
+@make_widget.when_type(TargetTextBox)
+def make_target_text_box(layout):
+    text_view = gtk.TextView()
+
+    add_spell_checking(text_view, pan_app.settings.language["contentlang"])
+
+    def get_range(buf, left_offset, right_offset):
+        return buf.get_text(buf.get_iter_at_offset(left_offset),
+                            buf.get_iter_at_offset(right_offset))
+
+    def on_text_view_n_press_event(text_view, event):
+        """Handle special keypresses in the textarea."""
+        # Automatically move to the next line if \n is entered
+
+        if event.keyval == gtk.keysyms.n:
+            buf = text_view.get_buffer()
+            if get_range(buf, buf.props.cursor_position-1, buf.props.cursor_position) == "\\":
+                buf.insert_at_cursor('n\n')
+                text_view.scroll_mark_onscreen(buf.get_insert())
+                return True
+        return False
+
+    def on_text_view_key_press_event(text_view, event, *_args):
+        if event.keyval == gtk.keysyms.Return or event.keyval == gtk.keysyms.KP_Enter:
+            layout = get_layout(text_view.parent)
+            if layout.next != None:
+                next_text_view = get_widget(layout.next).child
+                focus_text_view(next_text_view)
+
+            else:
+                #self.must_advance = True
+                text_view.parent.emit('key-press-event', event)
+            return True
+        return False
+
+    def on_change(buf):
+        layout.set_text(markup.unescape(buf.get_text(buf.get_start_iter(), buf.get_end_iter())))
+
+    buf = undo_buffer.add_undo_to_buffer(text_view.get_buffer())
+    undo_buffer.execute_without_signals(buf, lambda: buf.set_text(markup.escape(layout.get_text())))
+    buf.connect('changed', on_change)
+
+    text_view.set_editable(layout.editable)
+    text_view.set_wrap_mode(gtk.WRAP_WORD)
+    text_view.set_border_window_size(gtk.TEXT_WINDOW_TOP, 1)
+    text_view.connect('key-press-event', on_text_view_n_press_event)
+    text_view.connect('key-press-event', on_text_view_key_press_event)
+
+    scrolled_window = gtk.ScrolledWindow()
+    scrolled_window.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+    scrolled_window.add(text_view)
+
+    return post_make_widget(scrolled_window, {layout.name: scrolled_window}, layout)
+
+@make_widget.when_type(Comment)
+def make_comment(comment):
+    text_box, names = make_source_text_box(comment)
+    expander = label_expander.LabelExpander(text_box, comment.get_text)
+    return post_make_widget(expander, names, comment)
+
+@make_widget.when_type(Option)
+def make_option(option):
+    def on_toggled(widget, *_args):
+        if widget.get_active():
+            option.set_option(True)
+        else:
+            option.set_option(False)
+
+    check_button = gtk.CheckButton(label=option.label)
+    check_button.connect('toggled', on_toggled)
+    if option.get_option():
+        check_button.set_active(True)
+    return post_make_widget(check_button, {'option-%s' % option.name: check_button}, option)
 
 def get_source(unit, index):
     if unit.hasplural():

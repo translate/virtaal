47c5ded9a3a0ba19ae9ca6746f60eb26f527dce8
Wynand Winterbach <wynand@translate.org.za>
2008-07-28 08:31:44 +0000

    Refactoring and comment addition.
    svn path=/src/trunk/virtaal/; revision=7908

--- virtaal/terminology.py
+++ virtaal/terminology.py
@@ -31,30 +31,51 @@ import pan_app
 from support.memoize import memoize, invalidates_memoization
 from translate.search import match
 
-def add_to_store(unit_dict, unit_builder, store):
-    for unit in store.units:
-        key = unicode(unit.source)
-        if key not in unit_dict:
-            unit_dict[key] = unit_builder(unit)
-
 def get_terminology_directory():
     return pan_app.settings.general["termininology-dir"]
 
 def get_suggestion_store(lang_code):
-    store = factory.getclass("tmp.po")()
-    unit_dict = {}
-    for base, _dirnames, filenames in os.walk(path.join(get_terminology_directory(), lang_code)):
-        for filename in filenames:
-            try:
-                add_to_store(unit_dict, store.UnitClass.buildfromunit, factory.getobject(path.join(base, filename)))
-            except ValueError:
+    """Return a suggestion store which is an amalgamation of all the translation
+    stores under <termininology_directory>/<lang_code>."""
+    def combine_stores_in_directory(unit_builder, dirname):
+        def load_store(unit_dict, filename):
+            """Load filename as a store, and call add_units, which enumerates
+            the units in the store, adding each unit to unit_dict if the unit's
+            msgid is not already in unit_dict."""
+            def add_units(unit_dict, unit_builder, store):
+                for unit in store.units:
+                    key = unicode(unit.source)
+                    if key not in unit_dict:
+                        unit_dict[key] = unit_builder(unit)
+
+            try: # Try to load filename as a translation store...
+                add_units(unit_dict, unit_builder, factory.getobject(filename))
+            except ValueError: # If filename isn't a translation store, we just do nothing
                 pass
-    for unit in unit_dict.itervalues():
+    
+        unit_dict = {} # msgid -> unit mapping.
+        for base, _dirnames, filenames in os.walk(dirname):
+            for filename in filenames:
+                load_store(unit_dict, path.join(base, os.path.join(base, filename)))          
+        return unit_dict
+
+    store         = factory.getclass("tmp.po")()
+    dir_to_search = path.join(get_terminology_directory(), lang_code)
+    unit_builder  = store.UnitClass.buildfromunit
+    for unit in combine_stores_in_directory(unit_builder, dir_to_search).itervalues():
         store.addunit(unit)
     return store
 
 @memoize
 def get_terminology_matcher(lang_code):
+    """Return a terminology matcher based on a translation store which is an
+    amalgamation of all translation stores under 
+    <termininology_directory>/<lang_code>
+    
+    <termininology_directory> is the globally specified termininology directory.
+    <lang_code> is the supplied parameter.
+    
+    @return: a translate.search.match.terminologymatcher"""
     return match.terminologymatcher(get_suggestion_store(pan_app.settings.language["contentlang"]))
 
 @invalidates_memoization(get_terminology_matcher)

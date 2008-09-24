023272fd032031e604323c43aba44d0fd98cf78e
Friedel Wolff <friedel@translate.org.za>
2008-09-24 21:38:09 +0000

    Update tip now that we use Alt+Down to copy original to target (tips still unused at the moment)
    svn path=/src/trunk/virtaal/; revision=8471

--- virtaal/tips.py
+++ virtaal/tips.py
@@ -24,7 +24,7 @@
 tips = [
 _("At the end of a translation, simply press <Enter> to continue with the next one."),
 # l10n: Refer to the translation of "Copy to target" to find the appropriate shortcut key to recommend
-_("To copy the original string into the target field, simply press <Alt+C>."),
+_("To copy the original string into the target field, simply press <Alt+Down>."),
 _("When editing a fuzzy translation, the fuzzy marker will automatically be removed."),
 # l10n: Refer to the translation of "Fuzzy" to find the appropriate shortcut key to recommend
 _("To mark the current translation as fuzzy, simply press <Alt+U>."),

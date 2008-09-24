591c88783eb8324c7f62e9f84dd5ebf33c2a3340
Friedel Wolff <friedel@translate.org.za>
2008-09-24 21:32:12 +0000

    Grab the focus later - this seems to work more reliably
    svn path=/src/trunk/virtaal/; revision=8470

--- virtaal/widgets/entry_dialog.py
+++ virtaal/widgets/entry_dialog.py
@@ -30,13 +30,13 @@ def EntryDialog(title):
 
     entry = gtk.Entry()
     entry.show()
-    entry.grab_focus()
     entry.set_activates_default(True)
     dlg.vbox.pack_start(entry)
 
     dlg.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
     dlg.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
     dlg.set_default_response(gtk.RESPONSE_OK)
+    entry.grab_focus()
     response = dlg.run()
 
     text = None

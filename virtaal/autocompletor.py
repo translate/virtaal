cd5a3d975fbf3b6c002b1943c47e7c8dfacb91fc
Friedel Wolff <friedel@translate.org.za>
2008-09-29 13:18:16 +0000

    Massively improve the interaction with undo code.
    A suggestion should never go into the undo buffer, but accepting a suggestion should.
    
    svn path=/src/trunk/virtaal/; revision=8521

--- virtaal/autocompletor.py
+++ virtaal/autocompletor.py
@@ -129,7 +129,8 @@ class AutoCompletor(object):
             correction on.
             """
         id_dict_names = (
-            '_textbuffer_handler_ids',
+            '_textbuffer_insert_ids',
+            '_textbuffer_delete_ids',
             '_textview_button_press_ids',
             '_textview_focus_out_ids',
             '_textview_key_press_ids',
@@ -139,8 +140,12 @@ class AutoCompletor(object):
             if not hasattr(self, name):
                 setattr(self, name, {})
 
-        handler_id = textview.get_buffer().connect('insert-text', self._on_insert_text)
-        self._textbuffer_handler_ids[textview] = handler_id
+        buffer = textview.get_buffer()
+        handler_id = buffer.connect('insert-text', self._on_insert_text)
+        self._textbuffer_insert_ids[buffer] = handler_id
+
+        handler_id = buffer.connect('delete-range', self._on_delete_range)
+        self._textbuffer_delete_ids[buffer] = handler_id
 
         handler_id = textview.connect('button-press-event', self._on_textview_button_press)
         self._textview_button_press_ids[textview] = handler_id
@@ -158,8 +163,10 @@ class AutoCompletor(object):
 
     def _check_delete_selection(self, buffer):
         """Deletes the current selection if said selection was created by the auto-completor."""
-        if hasattr(buffer, '_remove_selection') and buffer._remove_selection:
+        suggestion = getattr(buffer, '_suggestion', None)
+        if suggestion:
             buffer.delete_selection(False, True)
+            buffer._suggestion = None
 
     def _on_insert_text(self, buffer, iter, text, length):
         if self.wordsep_re.match(text):
@@ -171,7 +178,7 @@ class AutoCompletor(object):
         if len(lastword) >= self.comp_len:
             completed_word, word_postfix = self.autocomplete(lastword)
             if completed_word == lastword:
-                buffer._remove_selection = False
+                buffer._suggestion = None
                 return
 
             if completed_word:
@@ -179,20 +186,36 @@ class AutoCompletor(object):
                 # Updating of the buffer is deferred until after this signal
                 # and its side effects are taken care of. We abuse
                 # gobject.idle_add for that.
-                def complete_text():
-                    buffer.props.text = u''.join([completed_prefix, postfix])
+                def suggest_completion():
+                    def insert_action():
+                        buffer.insert_at_cursor(word_postfix)
+                    buffer.handler_block(self._textbuffer_insert_ids[buffer])
+                    undo_buffer.execute_without_signals(buffer, insert_action)
+                    buffer.handler_unblock(self._textbuffer_insert_ids[buffer])
                     sel_iter_start = buffer.get_iter_at_offset(len(prefix))
-                    sel_iter_end   = buffer.get_iter_at_offset(len(completed_prefix))
+                    sel_iter_end   = buffer.get_iter_at_offset(len(prefix+word_postfix))
                     buffer.select_range(sel_iter_start, sel_iter_end)
-                    undo_buffer.merge_actions(buffer, len(prefix))
-                    buffer._remove_selection = True
+                    buffer._suggestion = (sel_iter_start, sel_iter_end)
                     return False
 
-                gobject.idle_add(complete_text)
+                gobject.idle_add(suggest_completion)
             else:
-                buffer._remove_selection = False
+                buffer._suggestion = None
         else:
-            buffer._remove_selection = False
+            buffer._suggestion = None
+
+    def _on_delete_range(self, buf, start_iter, end_iter):
+        # If we are deleting the suggestion, we don't want it in the undo_stack
+        suggestion = getattr(buf, '_suggestion', None)
+        if suggestion:
+            selection = buf.get_selection_bounds()
+            if selection and suggestion[0].equal(selection[0]) and suggestion[1].equal(selection[1]):
+                # Not pretty, but it works
+                getattr(buf, "__undo_stack").pop()
+                return False
+            else:
+                self._check_delete_selection(buf)
+        buf._suggestion = None
 
     def _on_textview_button_press(self, textview, event):
         self._check_delete_selection(textview.get_buffer())
@@ -207,18 +230,35 @@ class AutoCompletor(object):
         """Catch tabs to the C{gtk.TextView} and make it keep the current selection."""
         iters = textview.get_buffer().get_selection_bounds()
 
-        if event.keyval == gtk.keysyms.Tab and iters:
-            textview.get_buffer().select_range(iters[1], iters[1])
+        if not iters:
+            return False
+        if event.keyval == gtk.keysyms.Tab:
+            buf = textview.get_buffer()
+            completion = buf.get_text(iters[0], iters[1])
+            buf.delete(iters[0], iters[1])
+            buf.insert_at_cursor(completion)
             return True
+        elif event.state & gtk.gdk.CONTROL_MASK and \
+                event.keyval == gtk.keysyms.Z or event.keyval== gtk.keysyms.BackSpace:
+            # An undo/delete event will unselect the suggestion and make it hang 
+            # around. Therefore weneed to remove the suggestion manually.
+            self._check_delete_selection(textview.get_buffer())
+            return False
 
     def _remove_textview(self, textview):
         """Remove the given C{gtk.TextView} from the list of widgets to do
             auto-correction on.
             """
-        if not hasattr(self, '_textbuffer_handler_ids'):
+        if not hasattr(self, '_textbuffer_insert_ids'):
             return
         # Disconnect the "insert-text" event handler
-        textview.get_buffer().disconnect(self._textbuffer_handler_ids[textview])
+        buffer = textview.get_buffer()
+        buffer.disconnect(self._textbuffer_insert_ids[buffer])
+
+        if not hasattr(self, '_textbuffer_delete_ids'):
+            return
+        # Disconnect the "delete-range" event handler
+        buffer.disconnect(self._textbuffer_delete_ids[buffer])
 
         if not hasattr(self, '_textview_focus_out_ids'):
             return

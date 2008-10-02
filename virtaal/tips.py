79bd2ff666c9b63e7222135ab1832a61cebbaa82
Friedel Wolff <friedel@translate.org.za>
2008-10-02 07:46:04 +0000

    Mention moving in large steps. -outdated comment
    svn path=/src/trunk/virtaal/; revision=8603

--- virtaal/tips.py
+++ virtaal/tips.py
@@ -22,10 +22,10 @@
 
 tips = [
     _("At the end of a translation, simply press <Enter> to continue with the next one."),
-    # l10n: Refer to the translation of "Copy to target" to find the appropriate shortcut key to recommend
     _("To copy the original string into the target field, simply press <Alt+Down>."),
     _("When editing a fuzzy translation, the fuzzy marker will automatically be removed."),
     # l10n: Refer to the translation of "Fuzzy" to find the appropriate shortcut key to recommend
     _("To mark the current translation as fuzzy, simply press <Alt+U>."),
     _("Use Ctrl+Up or Ctrl+Down to move between translations."),
+    _("Use Ctrl+PgUp or Ctrl+PgDown to move in large steps between translations."),
 ]

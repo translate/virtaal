0c82fca6c2bda1e70e9f624b97490181a700e99e
Friedel Wolff <friedel@translate.org.za>
2009-01-12 16:07:35 +0000

    Add some syntax problems with the language includes, including pt-BR -> pt_BR
    svn path=/src/trunk/virtaal/; revision=9625

--- setup.py
+++ setup.py
@@ -158,26 +158,26 @@ def create_inno_script(name, _lib_dir, dist_dir, exe_files, other_files, version
 ; will be overwritten the next time py2exe is run!
 
 [Languages]
-Name: "en"; MessagesFile; "compiler:Default.isl"
-Name: "eu"; MessagesFile: Basque.isl"
-Name: "ca"; MessagesFile: Catalan.isl"
-Name: "cz"; MessagesFile: Czech.isl"
-Name: "da"; MessagesFile: Danish.isl"
-Name: "nl"; MessagesFile: Dutch.isl"
-Name: "fi"; MessagesFile: Finnish.isl"
-Name: "fr"; MessagesFile: French.isl"
-Name: "de"; MessagesFile: German.isl"
-Name: "he"; MessagesFile: Hebrew.isl"
-Name: "hu"; MessagesFile: Hungarian.isl"
-Name: "it"; MessagesFile: Italian.isl"
-Name: "nb"; MessagesFile: Norwegian.isl"
-Name: "pl"; MessagesFile: Polish.isl"
-Name: "pt-BR"; MessagesFile: BrazilianPortuguese.isl"
-Name: "pt"; MessagesFile: Portuguese.isl"
-Name: "ru"; MessagesFile: Russian.isl"
-Name: "sk"; MessagesFile: Slovak.isl"
-Name: "sl"; MessagesFile: Slovenian.isl"
-Name: "es"; MessagesFile: Spanish.isl"
+Name: "en"; MessagesFile: "compiler:Default.isl"
+Name: "eu"; MessagesFile: "compiler:Languages\Basque.isl"
+Name: "ca"; MessagesFile: "compiler:Languages\Catalan.isl"
+Name: "cz"; MessagesFile: "compiler:Languages\Czech.isl"
+Name: "da"; MessagesFile: "compiler:Languages\Danish.isl"
+Name: "nl"; MessagesFile: "compiler:Languages\Dutch.isl"
+Name: "fi"; MessagesFile: "compiler:Languages\Finnish.isl"
+Name: "fr"; MessagesFile: "compiler:Languages\French.isl"
+Name: "de"; MessagesFile: "compiler:Languages\German.isl"
+Name: "he"; MessagesFile: "compiler:Languages\Hebrew.isl"
+Name: "hu"; MessagesFile: "compiler:Languages\Hungarian.isl"
+Name: "it"; MessagesFile: "compiler:Languages\Italian.isl"
+Name: "nb"; MessagesFile: "compiler:Languages\Norwegian.isl"
+Name: "pl"; MessagesFile: "compiler:Languages\Polish.isl"
+Name: "pt_BR"; MessagesFile: "compiler:Languages\BrazilianPortuguese.isl"
+Name: "pt"; MessagesFile: "compiler:Languages\Portuguese.isl"
+Name: "ru"; MessagesFile: "compiler:Languages\Russian.isl"
+Name: "sk"; MessagesFile: "compiler:Languages\Slovak.isl"
+Name: "sl"; MessagesFile: "compiler:Languages\Slovenian.isl"
+Name: "es"; MessagesFile: "compiler:Languages\Spanish.isl"
 
 [Setup]
 AppName=%(name)s
@@ -193,6 +193,7 @@ DefaultGroupName=%(name)s
 OutputBaseFilename=%(name)s-%(version)s-setup
 ChangesAssociations=yes
 SetupIconFile=%(icon_path)s
+;WizardSmallImageFile=compiler:images\WizModernSmallImage13.bmp
 WizardImageFile=%(wizard_image)s
 
 [Files]''' % {

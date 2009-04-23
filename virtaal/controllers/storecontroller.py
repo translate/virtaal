37221ce251aa4592ca47d60eb7490b6ad9ed43c3
Walter Leibbrandt <walter@translate.org.za>
2009-04-23 11:06:35 +0000

    Added missing import.
    svn path=/src/trunk/virtaal/; revision=10891

--- virtaal/controllers/storecontroller.py
+++ virtaal/controllers/storecontroller.py
@@ -18,9 +18,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import re
-
 import gobject
+import logging
+import re
 
 from virtaal.common import GObjectWrapper
 from virtaal.models import StoreModel

cb775ba5ca80521d33dddb38d7a873ad6ab7da9e
Dwayne Bailey <dwayne@translate.org.za>
2009-06-11 18:57:00 +0000

    Enable placeable support for Java MessageFormat
    svn path=/src/trunk/virtaal/; revision=11510

--- virtaal/controllers/placeablescontroller.py
+++ virtaal/controllers/placeablescontroller.py
@@ -111,6 +111,10 @@ class PlaceablesController(BaseController):
             _('Placeholders (Python)'),
             _('Placeholders in Python strings')
         )
+        self.parser_info[general.JavaMessageFormatPlaceable.parse] = (
+            _('Placeholders (Java)'),
+            _('Placeholders in Java strings')
+        )
         self.parser_info[general.NumberPlaceable.parse] = (
             _('Numbers'),
             #l10n: 'decimal fractions' refer to numbers like 0.2 or 499,99

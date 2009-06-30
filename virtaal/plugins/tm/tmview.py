translate/virtaal

commit e6d9b44effc915390e96836378c3148adfbdcd87

Use the new reverse keyword for sort to avoid the extra reversing step (supported since Python 2.4)
svn path=/src/trunk/virtaal/; revision=9514

virtaal/plugins/tm/tmview.py
@@ -95,8 +95,7 @@ class TMView(BaseView, GObjectWrapper):
                 anything_new = True
         if not anything_new:
             return
-        rows.sort(key=lambda x: 'quality' in x and x['quality'] or 0)
-        rows.reverse()
+        rows.sort(key=lambda x: 'quality' in x and x['quality'] or 0, reverse=True)
         rows = rows[:self.max_matches]
 
         liststore.clear()

commit 14a22f51cff843d34472f34ba86a78d64e43f5c8

Add match at the end of list to get the desired sorting. This fixes an error introduced in r9514 when a .reverse() was changed to reverse=True in the sort function
svn path=/src/trunk/virtaal/; revision=11782

virtaal/plugins/tm/tmview.py
@@ -139,9 +139,10 @@ class TMView(BaseView, GObjectWrapper):
         anything_new = False
         for match in matches:
             if match['target'] not in curr_targets:
-                # Let's insert at the start to help with sort stability of the
-                # list (existing 100% will be above a new 100%
-                rows.insert(0, match)
+                # Let's insert at the end to prioritise existing matches over
+                # new ones. We rely on the guarantee of sort stability. This
+                # way an existing 100% will be above a new 100%.
+                rows.append(match)
                 anything_new = True
         if not anything_new:
             return
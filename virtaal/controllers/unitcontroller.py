4e4bffc7f569052bb90057880fdc104eb5c0fb00
Walter Leibbrandt <walter@translate.org.za>
2009-07-13 13:10:57 +0000

    UnitController.load_unit(): s/==/is/. It's safer.
    svn path=/src/trunk/virtaal/; revision=11861

--- virtaal/controllers/unitcontroller.py
+++ virtaal/controllers/unitcontroller.py
@@ -73,7 +73,7 @@ class UnitController(BaseController):
 
     # METHODS #
     def load_unit(self, unit):
-        if self.current_unit and self.current_unit == unit:
+        if self.current_unit and self.current_unit is unit:
             return self.view
         self.current_unit = unit
         self.nplurals = self.main_controller.lang_controller.target_lang.nplurals

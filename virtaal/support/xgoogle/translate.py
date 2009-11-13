45d301feee302f4c6f1bcc1951a0b6db78e177b8
Walter Leibbrandt <walter@translate.org.za>
2009-11-13 09:23:37 +0000

    Updated license notice to include Friedel's GPL'd modifications.
    svn path=/src/trunk/virtaal/; revision=13043

--- virtaal/support/xgoogle/translate.py
+++ virtaal/support/xgoogle/translate.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 #
-# Peteris Krumins (peter@catonmat.net)
-# http://www.catonmat.net  --  good coders code, great reuse
+# Copyright (c) 2009 Zuza Software Foundation
 #
-# http://www.catonmat.net/blog/python-library-for-google-translate/
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# Code is licensed under MIT license.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# This file incorporates work covered by the following copyright and
+# permission notice:
+#
+#   Peteris Krumins (peter@catonmat.net)
+#   http://www.catonmat.net  --  good coders code, great reuse
+#
+#   http://www.catonmat.net/blog/python-library-for-google-translate/
+#
+#   Code is licensed under MIT license.
 #
 
 from browser import Browser, BrowserError

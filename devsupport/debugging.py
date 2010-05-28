0fa417528bb6559c399219555031ee8897791963
Walter Leibbrandt <walter@translate.org.za>
2010-05-28 16:45:57 +0000

    Added log_args() and log_all_methods() decorators: Prints function calls with parameters and return value.
    svn path=/src/trunk/virtaal/; revision=14688

--- devsupport/debugging.py
+++ devsupport/debugging.py
@@ -18,11 +18,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import pdb
+import logging
 
 
 def debug_func(f):
+    """Function decorator to start a pdb shell before calling the decorated
+        function."""
     def debuggedf(*args, **kwargs):
+        import pdb
         pdb.set_trace()
         return f(*args, **kwargs)
     return debuggedf
+
+def log_args(fn, classname=None):
+    """Function decorator that logs the call of the decorated function with
+        arguments and return value."""
+    if classname is None:
+        classname = ''
+    else:
+        classname = classname + '.'
+
+    def logger_fn(*args, **kwargs):
+        args_s = ', '.join([repr(a) for a in args])
+        kwargs_s = ', '.join(("%s=%s" % (key, repr(value))) for key, value in kwargs.items())
+        if not args_s or not kwargs_s:
+            all_args = args_s + kwargs_s
+        else:
+            all_args = ', '.join([args_s, kwargs_s])
+        retval = fn(*args, **kwargs)
+        print '%s%s(%s) => %s' % (classname, fn.__name__, all_args, retval)
+        return retval
+
+    return logger_fn
+
+def log_all_methods(cls):
+    """Class decorator that applies the C{log_args} decorator to all of the
+        methods in the class."""
+    if not hasattr(cls, '__base__'):
+        raise TypeError('Not a class: %s' % (cls))
+    for attr in dir(cls):
+        method = getattr(cls, attr)
+        if attr.startswith('__') and attr.endswith('__') or getattr(method, 'im_class', None) is not cls:
+            continue
+        if callable(method):
+            setattr(cls, attr, log_args(method, classname=cls.__name__))
+    return cls

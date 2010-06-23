86c52cbf51ea87261a689be50e9d0af137ecae21
Walter Leibbrandt <walter@translate.org.za>
2010-06-23 16:11:20 +0000

    Reverted part of commit 14727 that introduced a rendering bug when pressing tab on the navigation combo.
    svn path=/src/trunk/virtaal/; revision=14780

--- virtaal/views/widgets/storetreeview.py
+++ virtaal/views/widgets/storetreeview.py
@@ -66,6 +66,7 @@ class StoreTreeView(gtk.TreeView):
         self.connect('key-press-event', self._on_key_press)
         self.connect("cursor-changed", self._on_cursor_changed)
         self.connect("button-press-event", self._on_button_press)
+        self.connect('focus-in-event', self.on_configure_event)
 
         # The following connections are necessary, because Gtk+ apparently *only* uses accelerators
         # to add pretty key-bindings next to menu items and does not really care if an accelerator

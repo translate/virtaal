3d198c62e532aee126d5e14c1c06a4e2dcdae860
Friedel Wolff <friedel@translate.org.za>
2010-06-30 14:21:23 +0000

    On Windows, download spell checkers on demand.  This fixes bug 1493.
    svn path=/src/trunk/virtaal/; revision=14812

--- virtaal/plugins/spellchecker.py
+++ virtaal/plugins/spellchecker.py
@@ -18,9 +18,62 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import logging
+import sys
 import os
+import os.path
+from cStringIO import StringIO
+import tarfile
+
+from virtaal.__version__ import ver as version
 from virtaal.common import pan_app
 from virtaal.controllers import BasePlugin
+from virtaal.support.httpclient import HTTPClient, HTTPRequest
+
+if not pan_app.DEBUG:
+    try:
+        import psyco
+    except:
+        psyco = None
+else:
+    psyco = None
+
+class SpellcheckClient(HTTPClient):
+    """
+    HTTP client to handle the communication between Virtaal and the server.
+    """
+
+    def __init__(self):
+        super(SpellcheckClient, self).__init__()
+        platform = sys.platform
+        if platform.startswith('linux'):
+            if os.path.isfile('/etc/lsb-release'):
+                try:
+                    lines = open('/etc/lsb-release').read().splitlines()
+                    for line in lines:
+                        if line.startswith('DISTRIB_DESCRIPTION'):
+                            distro = line.split('=')[-1]
+                            distro = distro.replace('"', '')
+                            platform = '%s; %s' % (platform, distro)
+                except Exception, e:
+                    pass
+        self.user_agent = 'Virtaal/%s (%s)' % (version, platform)
+
+    def get(self, url, callback, etag=None, error_callback=None):
+        headers = None
+        if etag:
+            # See http://en.wikipedia.org/wiki/HTTP_ETag for more details about ETags
+            headers = ['If-None-Match: "%s"' % (etag)]
+        request = HTTPRequest(url, headers=headers, user_agent=self.user_agent, follow_location=True)
+        self.add(request)
+
+        if callback:
+            request.connect('http-success', callback)
+            request.connect('http-redirect', callback)
+        if error_callback:
+            request.connect('http-client-error', error_callback)
+            request.connect('http-server-error', error_callback)
+
 
 class Plugin(BasePlugin):
     """A plugin to control spell checking.
@@ -30,6 +83,10 @@ class Plugin(BasePlugin):
     display_name = _('Spell Checker')
     description = _('Check spelling and provide suggestions')
     version = 0.1
+
+    _base_URL = 'http://dictionary.locamotion.org/hunspell/'
+    _dict_URL = _base_URL + '%s.tar.bz2'
+    _lang_list = 'languages.txt'
 
     # INITIALIZERS #
     def __init__(self, internal_name, main_controller):
@@ -43,6 +100,8 @@ class Plugin(BasePlugin):
 
         self.unit_view = main_controller.unit_controller.view
         self._connect_id = self.unit_view.connect('textview-language-changed', self._on_unit_lang_changed)
+        self.clients = {}
+        self.languages = set()
 
     def destroy(self):
         """Remove signal connections and disable spell checking."""
@@ -52,6 +111,90 @@ class Plugin(BasePlugin):
             self._disable_checking(text_view)
 
     # METHODS #
+    def _download_checker(self, language):
+        """A Windows-only way to obtain new dictionaries."""
+        if 'APPDATA' not in os.environ:
+            # We won't have an idea of where to save it, so let's give up now
+            return
+        if language in self.clients:
+            # We already tried earlier, or started the process
+            return
+        if not self.languages:
+            if self._lang_list not in self.clients:
+                # We don't yet have a list of available languages
+                url = self._base_URL + self._lang_list #index page listing all the dictionaries
+                callback = lambda *args: self._process_index(language=language, *args)
+                self.clients[self._lang_list] = SpellcheckClient()
+                self.clients[self._lang_list].get(url, callback)
+                # self._process_index will call this again, so we can exit
+            return
+
+        # Let's see if a dictionary is available for this language:
+        for l in self.languages:
+            if l == language or l.startswith(language+'_'):
+                self.clients[language] = None
+                logging.debug("Will use %s to spell check %s", l, language)
+                language = l
+                break
+        else:
+            # No dictionary available
+            # Indicate that we never need to try this language:
+            logging.debug("Found no suitable language for spell checking")
+            self.clients[language] = None
+            return
+
+       # Now download the actual files after we have determined that it is
+       # available
+        self.clients[language] = SpellcheckClient()
+        callback = lambda *args: self._process_tarball(language=language, *args)
+        url = self._dict_URL % language
+
+        if logging.root.level != logging.DEBUG:
+            self.clients[language].get(url, callback)
+        else:
+            def error_log(request, result):
+                logging.debug('Could not get %s: status %d' % (url, request.status))
+            self.clients[language].get(url, callback, error_callback=error_log)
+
+    def _tar_ok(self, tar):
+        # TODO: Verify that the tarball is ok:
+        # - only two files
+        # - must be .aff and .dic
+        # - language codes should be sane
+        # - file sizes should be ok
+        # - no directory structure
+        return True
+
+    def _ensure_dir(self, dir):
+        if not os.path.isdir(dir):
+            os.makedirs(dir)
+
+    def _process_index(self, request, result, language=None):
+        """Process the list of languages."""
+        if request.status == 200 and not self.languages:
+            self.languages = set(result.split())
+            self._download_checker(language)
+        else:
+            logging.debug("Couldn't get list of spell checkers")
+            #TODO: disable plugin
+
+    def _process_tarball(self, request, result, language=None):
+        # Indicate that we already tried and shouldn't try again later:
+        self.clients[language] = None
+
+        if request.status == 200:
+            logging.debug('Got a dictionary')
+            file_obj = StringIO(result)
+            tar = tarfile.open(fileobj=file_obj)
+            if not self._tar_ok(tar):
+                return
+            DICTDIR = os.path.join(os.environ['APPDATA'], 'enchant', 'myspell')
+            self._ensure_dir(DICTDIR)
+            tar.extractall(DICTDIR)
+            self.unit_view.update_languages()
+        else:
+            logging.debug("Couldn't get a dictionary. Status code: %d" % (request.status))
+
     def _disable_checking(self, text_view):
         """Disable checking on the given text_view."""
         spell = None
@@ -65,6 +208,8 @@ class Plugin(BasePlugin):
         if not spell is None:
             spell.detach()
         text_view.spell_lang = None
+    if psyco:
+        psyco.cannotcompile(_disable_checking)
 
 
     # SIGNAL HANDLERS #
@@ -86,6 +231,13 @@ class Plugin(BasePlugin):
                     break
             else:
                 #logging.debug('No code in enchant.list_languages() that starts with "%s"' % (language))
+
+                # If we are on Windows, let's try to download a spell checker:
+                if os.name == 'nt':
+                    self._download_checker(language)
+                    # If we get it, it will only be activated asynchronously
+                    # later
+                #TODO: packagekit on Linux?
 
                 # We couldn't find a dictionary for "language", so we should make sure that we don't
                 # have a spell checker for a different language on the text view. See bug 717.
@@ -112,9 +264,5 @@ class Plugin(BasePlugin):
             self.gtkspell = None
             #TODO: unload plugin
 
-    if not pan_app.DEBUG:
-        try:
-            import psyco
-            psyco.cannotcompile(_on_unit_lang_changed)
-        except ImportError, e:
-            pass
+    if psyco:
+        psyco.cannotcompile(_on_unit_lang_changed)

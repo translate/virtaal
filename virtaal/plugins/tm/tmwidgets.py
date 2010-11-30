translate/virtaal

commit 78d3fb2de5469e0b467d23f1afc739b2a1ad0695

Get rid of the flickering in the tm popup widget
svn path=/src/trunk/virtaal/; revision=16763

virtaal/plugins/tm/tmwidgets.py
@@ -111,8 +111,7 @@ class TMWindow(gtk.Window):
         # TODO: Replace the hard-coded value above with a query to the theme. It represents the width of the shadow of self.scrolled_window
 
         #logging.debug('TMWindow.update_geometry(%dx%d +%d+%d)' % (width, height, x, y))
-        self.set_size_request(width, height)
-        self.reshow_with_initial_size()
+        self.resize(width, height)
         self.move(x, y)
 
 

commit abaffce12f252f648fdc49c15609a6ea27e056d0

Move and resize the toplevel gdk window. This eliminates the sporadic issue with wrong sizes introduced in r16763.
svn path=/src/trunk/virtaal/; revision=16766

virtaal/plugins/tm/tmwidgets.py
@@ -111,8 +111,7 @@ class TMWindow(gtk.Window):
         # TODO: Replace the hard-coded value above with a query to the theme. It represents the width of the shadow of self.scrolled_window
 
         #logging.debug('TMWindow.update_geometry(%dx%d +%d+%d)' % (width, height, x, y))
-        self.resize(width, height)
-        self.move(x, y)
+        self.window.get_toplevel().move_resize(x,y, width,height)
 
 
     # EVENT HANLDERS #
1300cb090e60f81c0d50303397900c94ade54157
Friedel Wolff <friedel@translate.org.za>
2010-12-22 13:52:44 +0000

    -unused import [Walter Leibbrandt]
    svn path=/src/trunk/virtaal/; revision=16931

--- virtaal/views/widgets/textbox.py
+++ virtaal/views/widgets/textbox.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import gobject
 import gtk
 from gobject import SIGNAL_RUN_FIRST, SIGNAL_RUN_LAST
 

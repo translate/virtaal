4a9df9e091ff2d4b5305e2bc913dfa6ebd36b65b
Dwayne Bailey <dwayne@translate.org.za>
2011-05-25 18:43:33 +0000

    First stab at an automated build script
    svn path=/src/trunk/virtaal/; revision=17454

---
+++ devsupport/mac-bundle/builder.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Setup jhbuild your .jhbuild-custom should contain the following
+#setup_sdk(target="10.5", sdk_version="10.5", architectures=["i386"])
+#build_policy = "updated-deps"
+#moduleset="http://github.com/jralls/gtk-osx-build/raw/master/modulesets-stable/gtk-osx.modules"
+#_gtk_osx_use_jhbuild_python = True # Needed because we're going to bundle Virtaal
+
+jhbuild=~/.local/bin/jhbuild
+virtaal_mac=/Users/dev/gtk/source/virtaal-trunk/devsupport/mac-bundle/
+virtaal_modules=${virtaal_mac}/virtaal.modules
+
+# Actual build
+$jhbuild bootstrap --ignore-system
+$jhbuild --moduleset=$virtaal_modules build berkeleydb
+$jhbuild --moduleset=bootstrap.modules buildone --force python # This rebuilds python - oh well
+$jhbuild build meta-gtk-osx-bootstrap meta-gtk-osx-core meta-gtk-osx-python
+$jhbuild --moduleset=$virtaal_modules build virtaal
+
+# Test the build
+#jhbuild shell
+#virtaal
+
+# Buncle the build
+# TODO build and get ige-mac-bundler
+$jhbuild shell
+chmod +w $PREFIX/lib/libpython2.7.dylib
+ige-mac-bundler $virtaal_mac/virtaal.bundle
+
+# Make a .dmg file
+echo "Make dmg based on gramps guidelines"
+
+

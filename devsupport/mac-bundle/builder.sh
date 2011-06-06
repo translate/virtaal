c3d62346ed0803e581e6dbc0cfced9af1b8cafb8
Dwayne Bailey <dwayne@translate.org.za>
2011-06-06 07:07:23 +0000

    Add auto DMG building parts.  No matter how hard we try this doesn't seem to result in a pretty installer once it's in the dmg
    svn path=/src/trunk/virtaal/; revision=17529

--- devsupport/mac-bundle/builder.sh
+++ devsupport/mac-bundle/builder.sh
@@ -9,8 +9,11 @@
 jhbuild=~/.local/bin/jhbuild
 # For interactive builds set this to empty
 jhbuild_options="--no-interact"
-virtaal_mac=/Users/dev/gtk/source/virtaal-trunk/devsupport/mac-bundle/
-virtaal_modules=${virtaal_mac}/virtaal.modules
+src_dir=/Users/dev/gtk/source/virtaal-trunk/
+bundle_dir=/Users/dev/gtk/source/virtaal-trunk/devsupport/mac-bundle/
+virtaal_modules=${bundle_dir}/virtaal.modules
+app_name=Virtaal
+app_version=0.7.0
 
 # Actual build
 $jhbuild $jhbuild_options bootstrap --ignore-system &&
@@ -23,13 +26,52 @@ $jhbuild $jhbuild_options --moduleset=$virtaal_modules build virtaal
 #jhbuild shell
 #virtaal
 
-# Buncle the build
+
+## Bundle the build
 # TODO build and get ige-mac-bundler
 ige_mac_bundler=~/.local/bin/ige-mac-bundler
 $jhbuild run $SHELL -c 'chmod -w $PREFIX/lib/libpython2.7.dylib'
-$jhbuild run $SHELL -c "$ige_mac_bundler $virtaal_mac/virtaal.bundle"
-
-# Make a .dmg file
-echo "Make dmg based on gramps guidelines"
+$jhbuild run $SHELL -c "$ige_mac_bundler $bundle_dir/virtaal.bundle"
 
 
+## Make a .dmg file
+# Mostly taken from http://endrift.com/blog/2010/06/14/dmg-files-volume-icons-cli/
+volume_name="$app_name"
+dmg_file=~/Desktop/$app_name-$app_version-mac-beta-1.dmg
+raw_dmg_file=~/Desktop/raw-$app_name-$app_version-mac-beta-1.dmg
+tmp=~/Desktop/tmp-$app_name
+
+# Stage files
+rm -rf $tmp
+mkdir -p $tmp
+mv ~/Desktop/Virtaal.app $tmp
+cp -p $src_dir/README $tmp/ReadMe.txt
+cp -p $src_dir/LICENSE $tmp/License.txt
+(cd $tmp; ln -s /Applications)
+
+# Set background
+mkdir -p $tmp/.background
+cp -p $bundle_dir/virtaal_DMG_background.png $tmp/.background
+
+# Set Volume Icon
+cp $bundle_dir/icons/VolumeIcon_virtaal.icns $tmp/.VolumeIcon.icns
+SetFile -c icnC $tmp/.VolumeIcon.icns
+
+# Lets do some internal layout
+read -p "Pause to allow you to setup the folder" pause
+
+# Make an image
+hdiutil create -srcfolder $tmp -volname $volume_name -format UDRW -ov $raw_dmg_file
+
+# Sort out the Volume Icon
+rm -rf $tmp
+mkdir -p $tmp
+hdiutil attach $raw_dmg_file -mountpoint $tmp
+SetFile -a C $tmp
+hdiutil detach $tmp
+
+# Make final DMG
+rm -rf $tmp
+rm -f $dmg_file
+hdiutil convert $raw_dmg_file -format UDZO -imagekey zlib-level=9 -o $dmg_file 
+rm -f $raw_dmg_file

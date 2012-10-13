66c0408c1d0b959318db04187b9c4d2416374ac4
Pavol Babincak <scroolik@gmail.com>
2012-10-13 12:18:06 +0200

    Removed tests from virtaal/test/test_unitcontroller.py
    - Test case test_load_unit was testing code which was removed in SVN
      rev. 9941:
      83dc015 Major changes to use only one UnitView to render all units.
    
    - Second part of test case test_set_target was testing a buffer code of
      gtk.TextViews which was replaced  by TextBox in SVN rev. 10488:
      f24b934 Replace gtk.TextViews with our shiny new TextBox class. :)
    
    - Second assert in test case test_get_target was only rewritten
      definition of get_unit_target():
        def get_unit_target(self, target_index):
            return self.view.get_target_n(target_index)

--- virtaal/test/test_unitcontroller.py
+++ virtaal/test/test_unitcontroller.py
@@ -22,30 +22,14 @@ from test_scaffolding import TestScaffolding
 
 
 class TestUnitController(TestScaffolding):
-    def test_load_unit(self):
-        test_unit = self.trans_store.getunits()[1]
 
-        view = self.unit_controller.load_unit(test_unit)
-        assert view is self.unit_controller.view
-        assert view in self.unit_controller.unit_views.values()
-        assert len(self.unit_controller.unit_views) == 1
 
-        # Make sure that we don't create more than one view for the same unit
-        view = self.unit_controller.load_unit(test_unit)
-        assert len(self.unit_controller.unit_views) == 1
 
     def test_get_target(self):
         # The unit indexes below differ by 1, because the StoreModel class (and thus the rest of Virtaal)
         # ignores PO headers (and other untranslatable units), whereas the Toolkit's stores do not.
         assert self.unit_controller.get_unit_target(0) == self.trans_store.getunits()[1].target
-        assert self.unit_controller.get_unit_target(0) == self.unit_controller.view.get_target_n(0)
 
     def test_set_target(self):
         self.unit_controller.set_unit_target(0, 'Test')
         assert self.unit_controller.get_unit_target(0) == 'Test'
-
-        target = self.unit_controller.view.targets[0]
-        tv_buff = target.get_buffer()
-        tv_text = tv_buff.get_text(tv_buff.get_start_iter(), tv_buff.get_end_iter())
-        assert tv_text == 'Test'
-        assert self.unit_controller.view.is_modified()

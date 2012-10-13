2c3f060060c12457fa2d12e2845c245cb2760c6e
Pavol Babincak <scroolik@gmail.com>
2012-10-13 12:15:52 +0200

    py.test on virtaal/test/test_storecontroller.py working again

--- virtaal/test/test_storemodel.py
+++ virtaal/test/test_storemodel.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-from virtaal.models import StoreModel
+from virtaal.models.storemodel import StoreModel
 
 from test_scaffolding import TestScaffolding
 

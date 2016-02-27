a0feb327d5bcd207efddfc508f51bb1c2df76cb4
Friedel Wolff <friedel@translate.org.za>
2016-02-27 12:30:31 +0200

    Avoid error when closing a big file while loading

--- virtaal/plugins/tm/tmcontroller.py
+++ virtaal/plugins/tm/tmcontroller.py
@@ -198,6 +198,10 @@ class TMController(BaseController):
     # EVENT HANDLERS #
     def _on_cursor_changed(self, cursor):
         self.storecursor = cursor
+        if cursor is None:
+            # this can happen if we close a big file before it finished loading
+            return
+
         self.unit = cursor.deref()
 
         if self.unit is None:
